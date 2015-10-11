516714cb700ca25231c11e87c915685e4a6e4890
Mark Mulder <markmulder@gmail.com>
2015-10-11 19:17:53 +0100

    Refactor to method

--- udp_listener.go
+++ udp_listener.go
@@ -39,10 +39,27 @@ type Incoming struct {
 	Data       string    `db:"data"`
 }
 
-func (n *Notifier) GetRules() []*Rule {
+func (n *Notifier) getRules() []*Rule {
 	rules := []*Rule{}
 	n.Rules.Unmarshal(&rules)
 	return rules
+}
+
+func (n *Notifier) checkRules(s *Stat) bool {
+	rules := n.getRules()
+	rules_met := true
+	for _, rule := range rules {
+		if !rule.Met(s) {
+			fmt.Printf("Rule not met -- Key: %s, Type: %s, Setting %s, Value %s\n", rule.Key, rule.Type, rule.Setting, rule.Value)
+			rules_met = false
+		}
+	}
+	if !rules_met {
+		fmt.Printf("Stopping notification of id: %d, rules not met\n", n.Id)
+		return false
+	}
+
+	return true
 }
 
 func (i *Incoming) FormattedData() string {
@@ -79,16 +96,7 @@ Notify:
 		nt := notifier.NotificationType
 		fmt.Printf("Notifying notifier id: %d type: %s\n", notifier.Id, nt)
 
-		rules := notifier.GetRules()
-		rules_met := true
-		for _, rule := range rules {
-			if !rule.Met(s) {
-				fmt.Printf("Rule not met -- Key: %s, Type: %s, Setting %s, Value %s\n", rule.Key, rule.Type, rule.Setting, rule.Value)
-				rules_met = false
-			}
-		}
-		if !rules_met {
-			fmt.Printf("Stopping notification of id: %d, rules not met\n", notifier.Id)
+		if !notifier.checkRules(s) {
 			continue Notify
 		}
 
@@ -162,5 +170,4 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe ", err)
 	}
-
 }

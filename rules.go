c6edd06452b5a1ee6c04122302b2f40eb7defff4
Mark Mulder <markmulder@gmail.com>
2015-10-11 19:17:53 +0100

    Refactor to case

--- rules.go
+++ rules.go
@@ -26,11 +26,12 @@ func (r *Rule) Met(s *Stat) bool {
 		return false
 	}
 
-	if r.Type == "boolean" {
+	switch r.Type {
+	case "boolean":
 		return metBool(r, parsed)
-	} else if r.Type == "string" {
+	case "string":
 		return metString(r, parsed)
-	} else if r.Type == "number" {
+	case "number":
 		return metNumber(r, parsed)
 	}
 
@@ -61,15 +62,16 @@ func metNumber(r *Rule, parsed map[string]interface{}) bool {
 	val := parsed[r.Key].(float64)
 	needed_val, _ := strconv.ParseFloat(r.Value, 64)
 
-	if r.Setting == "eq" {
+	switch r.Setting {
+	case "eq":
 		if val != needed_val {
 			return false
 		}
-	} else if r.Setting == "gt" {
+	case "gt":
 		if val <= needed_val {
 			return false
 		}
-	} else if r.Setting == "lt" {
+	case "lt":
 		if val >= needed_val {
 			return false
 		}

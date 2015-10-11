19ad9aeebaf4b94b425bd6c42615c26c69d8eab9
Mark Mulder <markmulder@gmail.com>
2015-10-11 19:17:53 +0100

    Minor cleanup and http endpoint on /

--- udp_listener.go
+++ udp_listener.go
@@ -4,12 +4,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	// "github.com/hoisie/redis"
 	// _ "github.com/jmoiron/sqlx"
 	"github.com/jmoiron/sqlx/types"
 	_ "github.com/lib/pq"
 	"log"
 	"net"
+	"net/http"
 	"time"
 )
 
@@ -23,47 +23,25 @@ type Stat struct {
 }
 
 func (s *Stat) persist() {
-	fmt.Println(s.Key)
-	fmt.Println(s.Value)
 	var incomingId int
-	// err := db.QueryRow(`INSERT INTO incoming(received_at, data) VALUES(?, ?)`, time.Now(), s.Value).Scan(&incomingId)
 	err := db.QueryRow(`INSERT INTO incoming(received_at, data) VALUES($1, $2) RETURNING id`, time.Now(), s.Value).Scan(&incomingId)
 	if err != nil {
 		log.Fatal("persist()", err)
 	}
-	fmt.Println("saved to id: ", incomingId)
+	fmt.Printf("class: %s id: %d\n", s.Key, incomingId)
 }
 
-func trackStat(stat Stat) {
-	// var client redis.Client
-	log.Printf("%+v\n", stat.Key)
-	log.Printf("%+v\n", stat.Value)
-	// client.Lpush("marktest", []byte(stat.Value))
-}
-
-func main() {
-	addr, err := net.ResolveUDPAddr("udp", ":8000")
-	if err != nil {
-		log.Fatal("ResolveUDPAddr", err)
-	}
-	conn, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		log.Fatal("ListenUDP", err)
-	}
-
-	db, err = sql.Open("postgres", "user=markmulder dbname=notifier sslmode=disable")
-	if err != nil {
-		log.Fatal("DB Open()", err)
-	}
-	defer db.Close()
-
+func countRows() int {
 	var rows int
-	err = db.QueryRow("select count(*) from incoming").Scan(&rows)
+	err := db.QueryRow("select count(*) from incoming").Scan(&rows)
 	if err != nil {
 		log.Fatal("rowcount: ", err)
 	}
-	fmt.Println("== rows: ", rows)
 
+	return rows
+}
+
+func listenToUDP(conn *net.UDPConn) {
 	buffer := make([]byte, maxPacketSize)
 	for {
 		bytes, err := conn.Read(buffer)
@@ -82,9 +60,54 @@ func main() {
 			log.Printf("%+v\n", stat)
 		}
 
-		trackStat(stat)
 		stat.persist()
 	}
+}
 
-	fmt.Println("Listening from here")
+func handleCount(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	count := countRows()
+	jmap := map[string]int{
+		"status": 200,
+		"count":  count,
+	}
+
+	output, err := json.MarshalIndent(jmap, "", "  ")
+	if err != nil {
+		log.Fatal("MarshalIndent", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write(output)
+}
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp", ":8000")
+	if err != nil {
+		log.Fatal("ResolveUDPAddr", err)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatal("ListenUDP", err)
+	}
+
+	db, err = sql.Open("postgres", "user=markmulder dbname=notifier sslmode=disable")
+	if err != nil {
+		log.Fatal("DB Open()", err)
+	}
+	defer db.Close()
+
+	rows := countRows()
+	fmt.Println("Total rows: ", rows)
+
+	go listenToUDP(conn)
+	http.HandleFunc("/", handleCount)
+
+	fmt.Println("Will start listening on port 8000")
+	http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe ", err)
+	}
+
 }

5d46eab3d7226abbf87f4ed67692ecea8ca8261e
Mark Mulder <markmulder@gmail.com>
2015-10-11 19:17:53 +0100

    Fix flickering spec by sorting the array

--- stat_test.go
+++ stat_test.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"sort"
 	"testing"
 
+	"github.com/jmoiron/sqlx/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStatPersist(t *testing.T) {
 	var originalCount int
-	_ = db.QueryRow("select count(*) from incoming").Scan(&originalCount)
+	_ = db.QueryRow("SELECT COUNT(*) FROM incoming").Scan(&originalCount)
 
 	s := Stat{"Mark", jt}
 	s.persist()
 
 	var newCount int
-	_ = db.QueryRow("select count(*) from incoming").Scan(&newCount)
+	_ = db.QueryRow("SELECT COUNT(*) FROM incoming").Scan(&newCount)
 	assert.Equal(t, newCount, originalCount+1)
 }
 
 func TestStatKeys(t *testing.T) {
+	var jt = types.JsonText(`{"active": true, "name": "Go", "number": "12"}`)
+
 	s := Stat{"Mark", jt}
-	keys := s.keys()
+	result := s.keys()
 	expected := []string{"active", "name", "number"}
-	assert.Equal(t, expected, keys)
+
+	sort.Strings(result)
+	sort.Strings(expected)
+	assert.Equal(t, expected, result)
 }
